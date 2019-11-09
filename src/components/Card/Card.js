84bf3a11a13e499f2b85da3e4f510a2b5aa2e6b9
Nahuel Carreiro <nahuel.c@geopagos.com>
2019-11-09 16:12:29 +0100

    Fixed PropTypes of Card + added ESLint Jest plugin for tests.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const CardContainer = styled.div`
   border: 2px solid #333;
@@ -12,3 +13,7 @@ export default class Card extends Component {
     return <CardContainer>{text}</CardContainer>;
   }
 }
+
+Card.propTypes = {
+  text: PropTypes.string.isRequired
+};
